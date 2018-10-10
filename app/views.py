774add3c08f38d62680ff2844f825bd8fd185c42
nixis-institute <nixis.institute@gmail.com>
2018-10-10 12:58:02 +0530

    update

--- app/views.py
+++ app/views.py
@@ -9,6 +9,8 @@ def job_application (request):
     job = job_application.objects.all()
     return render_to_response("job_application.html",{"job":job})
 def home (request):
-    return render_to_response("home.html")
+    data = JOB_POSTING.objects.all()
+    return render_to_response('home.html',{"job":data})
 def layout (request):
     return render_to_response ("layout.html")
+ 
--- jobhub/urls.py
+++ jobhub/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from app.views import *
-#from app.views import *
+from app.models import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
